26641a47569cccd19bae1e7ce2bfa9e4371dd597
Luciano Campos <lucianobandeiracampos@gmail.com>
2020-11-30 15:47:54 -0300

    db ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
